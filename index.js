2eab02ba6f68e2a6643534545abf0af5ebd94ab3
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-04 11:59:06 -0400

    commit tree 0f1e4fd8879c52a686c58b736827a2cd997d8d34

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json());
-  
-  renderBooks(resp);
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(
+    function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+      renderBooks(json);
+    });
 
 }
 
